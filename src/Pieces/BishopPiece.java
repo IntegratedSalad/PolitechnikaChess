ffdcb9bd1746942411605f57d0e0efc03a15c34f
Adam Trzpis <worms3d4@tlen.pl>
2024-05-23 16:48:59 +0200

    + Moving the Bishop

--- src/Pieces/BishopPiece.java
+++ src/Pieces/BishopPiece.java
@@ -4,6 +4,8 @@ import Chess.Color;
 import Chess.Piece;
 import Chess.Pos;
 
+import static java.lang.Math.*;
+
 public class BishopPiece implements Piece {
     /* Attributes */
     private Color color = null;
@@ -17,9 +19,38 @@ public class BishopPiece implements Piece {
     }
 
     public boolean DoMove(final Piece[][] mapArray, final Pos displacementPos) {
+        System.out.println("Movinge binshopee :DDDD");
+        final int pieceX = pos.GetX();
+        final int pieceY = pos.GetY();
+        final int dispX = displacementPos.GetX();
+        final int dispY = displacementPos.GetY();
+//        final double length = floor(sqrt(pow(dispX - pieceX, 2) + pow(dispY - pieceY, 2)));
 
+        final int dirX = (dispX - pieceX) / abs(dispX - pieceX);
+        final int dirY = (dispY - pieceY) / abs(dispY - pieceY);
 
-        return true;
+        System.out.println("pieceX: " + pieceX + " pieceY:" + pieceY);
+        System.out.println("dispX: " + dispX + " dispY:" + dispY);
+        System.out.println("dirX: " + dirX + " dirY:" + dirY);
+
+        if ((dirX == 1 && dirY == 1) ||
+           (dirX == -1 && dirY == -1) ||
+           (dirX == 1 && dirY == -1) ||
+           (dirX == -1 && dirY == 1)) { // only diagonally
+            int x = pieceX;
+            int y = pieceY;
+            System.out.println("Diagonal move!");
+            while (x != dispX && y != dispY) {
+                if (!(mapArray[y][x] instanceof EmptyPiece)) {
+                    System.out.println("Something's in the way...");
+                    return false;
+                }
+                x += dirX;
+                y += dirY;
+            }
+            return true;
+        }
+        return false;
     }
     public String RepresentOnBoard() {
         char colorGlyph;
